490643116309e81d4bf3cf6491ea1a4b7da071e4
Henrique <henrique.balbino.silva@hotmail.com>
2014-09-27 12:05:43 -0300

    Aprendendo

--- Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -10,11 +10,11 @@ namespace Quiron.LojaVirtual.Web.Controllers
     public class ProdutosController : Controller
     {
         private ProdutosRepositorio _repositorio;
-        // GET: Produtos
         public ActionResult Index()
         {
            _repositorio = new ProdutosRepositorio();
-           var produtos = _repositorio.Produtos.Take(10);
+            var produtos = _repositorio.Produtos;
+
 
             return View(produtos);
         }
