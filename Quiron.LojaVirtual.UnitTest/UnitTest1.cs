27d493017897f0583e086ab5a6931eb8a8cc1bfa
Henrique <henrique.balbino.silva@hotmail.com>
2014-10-07 17:29:28 -0300

    Paginação  View Result

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -7,8 +11,32 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTestQuiron
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestarSeAPaginacaoEstaSendoGeradaCorretamente()
         {
+          //Arrange
+
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensTotal = 10,
+                ItensPorPagina = 10
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            Assert.AreEqual(
+
+                @"<a class=""btn btn-default"" href=""Pagina1"">'</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>",resultado.ToString()
+
+                );
         }
+
+     
     }
 }
---
+++ Quiron.LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Collections.Generic;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class ProdutosViewModel
+    {
+
+
+        public IEnumerable<Produto> Produtos { get; set; }
+        public Paginacao Paginacao { get; set; }
+
+
+    }
+}
